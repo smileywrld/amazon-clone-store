d7fab7d22c112e244e805e2272af136e3c357424
smileywrld <ismaileyyunusa@gmail.com>
2025-03-14 22:45:36 +0100

    Fix import paths and add currency formatting utility in checkout.js

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-import { products } from "./products";
+import { products } from "./products.js";
 
 export const cart = [
 	{
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import { cart } from "../data/cart.js";
 import { products } from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
 
 let cartSummaryHTML = "";
 
@@ -22,13 +23,12 @@ cart.forEach((cartItem) => {
             <div class="cart-item-details-grid">
               <img class="product-image"
                 src="${matchingProduct.image}">
-
               <div class="cart-item-details">
                 <div class="product-name">
                   ${matchingProduct.name}
                 </div>
                 <div class="product-price">
-                  $${matchingProduct.price / 100}
+                  $${formatCurrency(matchingProduct.priceCents)}
                 </div>
                 <div class="product-quantity">
                   <span>
