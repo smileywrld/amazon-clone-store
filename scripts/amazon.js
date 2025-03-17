672660e6ec721bfc50aaab910e22a14522e95e00
smileywrld <ismaileyyunusa@gmail.com>
2025-03-17 23:21:48 +0100

    Refactor price display in amazon.js to use currency formatting utility

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,48 +1,6 @@
-/*const products = [
-	{
-		image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-		name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-		rating: {
-			stars: 4.5,
-			count: 87,
-		},
-		priceCents: 1090,
-	},
-
-	{
-		image: "images/products/intermediate-composite-basketball.jpg",
-		name: "Intermediate Size Basketball",
-		rating: {
-			stars: 4,
-			count: 127,
-		},
-		priceCents: 2095,
-	},
-
-	{
-		image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-		name: "Adults Plain Cotton T-Shirt - 2 Pack",
-		rating: {
-			stars: 4.5,
-			count: 56,
-		},
-		priceCents: 799,
-	},
-
-	{
-		image: "images/products/black-2-slot-toaster.jpg",
-		name: "2 slot Toaster - Black",
-		rating: {
-			stars: 5,
-			count: 2197,
-		},
-
-		priceCents: 1899,
-	},
-]; */
-
 import { cart, addToCart } from "../data/cart.js";
 import { products } from "../data/products.js";
+import { formatCurrency } from "./utils/money.js";
 
 let productsHTML = "";
 
@@ -67,7 +25,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-price">
-            $${(product.priceCents / 100).toFixed(2)}
+            $${formatCurrency(product.priceCents)}
           </div>
 
           <div class="product-quantity-container">
