4dc7a42f9c1d94e9a02c638419d197996659ed9b
smileywrld <ismaileyyunusa@gmail.com>
2025-03-18 00:09:04 +0100

    Implement removeFromCart function and integrate with delete link event listener in checkout.js

--- data/cart.js
+++ data/cart.js
@@ -1,6 +1,6 @@
 import { products } from "./products.js";
 
-export const cart = [
+export let cart = [
 	{
 		productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
 		quantity: 2,
@@ -28,4 +28,16 @@ export function addToCart(productId) {
 			quantity: 1,
 		});
 	}
+}
+
+export function removeFromCart(productId) {
+	const newCart = [];
+
+	cart.forEach((cartItem) => {
+		if (cartItem.productId !== productId) {
+			newCart.push(cartItem);
+		}
+	});
+
+	cart = newCart;
 }
